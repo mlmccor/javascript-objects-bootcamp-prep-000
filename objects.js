24e2fb91c02f80a8e7420c4c807d6c909699bbf6
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-14 21:09:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,6 @@
  var playlist= {
    'Chicago': "Saturday", 'DaveMatthewsBand': "Too Much"}
-function updatePlaylist(playlist, artist, title) {
-  playlist[artist]= title
-  return playlist
+
 }
 function removeFromPlaylist(playlist, artist) {
   delete playlist.artist
